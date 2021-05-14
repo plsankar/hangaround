63a3208d343f95fb5a057b0b0b6ca951491db6ce
plsankar <plsankar1996@gmail.com>
2021-05-14 21:40:45 +0530

    fix: Fixed SiteItems Re-Rendering

--- src/Dashboard/SiteItem.tsx
+++ src/Dashboard/SiteItem.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import _ from 'lodash';
 import { Site } from '../types';
 import './SiteItem.scss';
 
@@ -9,12 +10,13 @@ type SiteItemProps = {
 const SiteItem: React.FC<SiteItemProps> = ({ site }: SiteItemProps) => {
     const { domain, duration } = site;
     const pad = (input: number) => input.toString().padStart(2, '0');
-    const time = () => {
+    const time = React.useCallback(() => {
         const hours: number = Math.floor(duration / 60 / 60);
         const minutes: number = Math.floor(duration / 60) - hours * 60;
         const seconds: number = duration % 60;
         return `${pad(hours)}h ${pad(minutes)}m ${pad(seconds)}s`;
-    };
+    }, [duration]);
+
     return (
         <div className="siteitem">
             <div className="siteitem__name">{domain}</div>
@@ -23,4 +25,6 @@ const SiteItem: React.FC<SiteItemProps> = ({ site }: SiteItemProps) => {
     );
 };
 
-export default SiteItem;
+export default React.memo(SiteItem, (prevProps, nextProps) => {
+    return _.isEqual(prevProps, nextProps);
+});
